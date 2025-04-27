10be2198eb0ed8f74fae3dadca0ceceb8ec2dbc7
Jonas Stamm <stammi92@gmail.com>
2025-04-27 19:52:12 +0200

    Upgraded strapi and fixed warnings and deps

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,12 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'e1f2d3c4b5a6978877665544332211ff')
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT', 'aabbccddeeff00112233445566778899')
+    }
+  }
 });
--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS')
+    keys: env.array('APP_KEYS', ['b7f8c2e1d4a5f6b7c8d9e0f1a2b3c4d5', 'e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1'])
   },
 });
